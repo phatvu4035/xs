f24e18010309ca88366b210b1cb6073883ad7cb9
vvphat <phatvu050992@gmail.com>
2021-02-20 11:36:24 +0700

    check interface

--- js/thongke-chu-ky-dan-dac-biet.js
+++ js/thongke-chu-ky-dan-dac-biet.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
-    $("#end_date").datepicker({ autoclose: true, language: "vi", format: "d-m-yyyy", weekStart: 1, endDate: new Date(), todayBtn: "linked", todayHighLight: true, startDate: "01-01-2001" });
-    link_selector_dpicker($("form#chuky_form select#code"), $("form#chuky_form input#end_date"));
-    disable_combine("mb", $("form#chuky_form input#end_date"));
-    $("#end_date").datepicker("update", "19-01-2021");
+    $("#begin_date").datepicker({ autoclose: true, language: "vi", format: "d-m-yyyy", endDate: new Date(), todayBtn: "linked", todayHighLight: true, startDate: "-14y" });
+    link_selector_dpicker($("form#main_form select#code"), $("form#main_form input#begin_date"));
+    disable_combine("mb", $("form#main_form input#begin_date"));
+    $("#begin_date").datepicker("update", "01-01-2005");
+    $("#end_date").datepicker({ autoclose: true, language: "vi", format: "d-m-yyyy", endDate: new Date(), todayBtn: "linked", todayHighLight: true, startDate: "-4y" });
+    link_selector_dpicker($("form#main_form select#code"), $("form#main_form input#end_date"));
+    disable_combine("mb", $("form#main_form input#end_date"));
+    $("#end_date").datepicker("update", "19-02-2021");
 });
